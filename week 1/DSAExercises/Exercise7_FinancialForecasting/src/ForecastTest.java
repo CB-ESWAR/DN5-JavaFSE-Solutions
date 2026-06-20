d740f83b8b3cf38eee79fde4d8b752a7e7c0320c
CB-ESWAR <cbeswar20072006@gmail.com>
2026-06-20 22:02:02 +0530

    Completed DSA Exercises 5 to 7

---
+++ week 1/DSAExercises/Exercise7_FinancialForecasting/src/ForecastTest.java
@@ -0,0 +1,18 @@
+public class ForecastTest {
+
+    public static void main(String[] args) {
+
+        double amount = 10000;
+        double rate = 0.10;
+        int years = 5;
+
+        double result =
+                FinancialForecast.futureValue(
+                        amount,
+                        rate,
+                        years);
+
+        System.out.println("Future Value = "
+                + result);
+    }
+}
